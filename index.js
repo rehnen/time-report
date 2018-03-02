149b37c183b71f4c71a3760c5f1cee54824d8841
Marcus Rehn <marcus.rehn@hotmail.com>
2018-03-02 21:40:58 +0100

    installed and played with koa-bodyparser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 const json = require('koa-json');
 
 const app = new Koa();
@@ -11,11 +12,27 @@ let times = [];
 
 router.get('/', (ctx, next) => {
   // ctx.router available
-  ctx.body = {name: "potato"};
+  ctx.body = { times };
+});
+
+router.put('/:date', (cxt, next) => {
+  console.log(Object.keys(cxt.request))
+  console.log(cxt.request.body);
+
+  times.push(cxt.request.body);
+
+
+//  console.log(next);
+  cxt.body = { times }
 });
 
 app.use(json())
-    .use(router.routes())
-    .use(router.allowedMethods());
+  .use(bodyParser({
+      extendTypes: {
+        json: ['application/x-javascript'] // will parse application/x-javascript type body as a JSON string
+      }
+  }))
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000);
